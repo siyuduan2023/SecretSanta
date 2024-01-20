d649d8b6c34ea5154935c9ee83d63e7f1eb36442
bidhan-vhadra <bvhadra@gmail.com>
2024-01-20 15:46:32 +0000

    Likes component created

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,14 @@
+export default function Likes(){
+    return(
+
+        <div className = "likes">
+        <form>
+        <h3>Likes</h3>
+        <input type="text" id="name" />
+        <input type="text" id="name" />
+        <input type="text" id="name" />
+</form>
+</div>
+    );
+
+}
